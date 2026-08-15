7816dc92cf22936c6b9ab808af7a6070c72772f8
EDT-ELT-DEPARTMENT <milouafarid@gmail.com>
2026-08-15 02:43:33 +0100

    Update app_surveillance_examens.py

--- app_surveillance_examens.py
+++ app_surveillance_examens.py
@@ -1,3 +1,8 @@
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 import streamlit as st
 import pandas as pd
